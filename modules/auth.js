e347e5af78bcd652091dc237db87cfd54b479380
Zohaib Mughal <z.mughal.24648@khi.iba.edu.pk>
2024-03-23 16:29:30 +0500

    Finalised , Login , Logout , Change Password , Register , Update User Profile . Authenticate Token , Require Role , Geet User Profile ,  Updated User Schema , Updated Comments Schema , Added Soft Delete ,

--- modules/auth.js
+++ modules/auth.js
@@ -5,11 +5,52 @@ const mongoose = require('mongoose');
 const router = express.Router();
 router.use(express.json());
 module.exports = router; // Export the router
-const Schema = mongoose.Schema;
-
-
 const securitykey ="ZohaibMughal";
 
+
+
+function decodeToken(token) {
+  try {
+    // Verify the token using the secret key and extract the payload
+    const decoded = jwt.verify(token, securitykey);
+    return decoded;
+  } catch (error) {
+    console.error("Error decoding token: ", error.message);
+    return null;
+  }
+}
+
+
+function authenticateToken(req, res, next) {
+  // Extract the token from the Authorization header and expect a Bearer token
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  
+  if (!token) {
+    return res.status(401).json({ msg: "Access Denied: No token provided" });
+  }
+
+  jwt.verify(token, securitykey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ msg: "Invalid Token" });
+    }
+    req.user = user; // Attach the user payload to the request
+    console.log('authenticated user having token ' + token);
+    next(); // Proceed to the next middleware or route handler
+  });
+}
+
+function requireRole(role) {
+  return function(req, res, next) {
+    // Assuming req.user.roles is an array of roles
+    if (!req.user.roles.includes(role)) {
+      return res.status(403).send({ message: `Access denied. Requires ${role} role.` });
+    }
+    next();
+  };
+}
+
+// this will be modified to only accept emails of IBA
 const emailValidator = {
     validator: function(value) {
         const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
@@ -98,12 +139,31 @@ const Teachers = mongoose.model('Teachers', teacherSchema);
         unique:true,
         validate: erpValidator
     },
+    profile_picture : String,
     
-    roles : {type:String,
-    default:"User"}
+    roles: {
+      type: [String], 
+      default: ["User"] 
+    },
+
+    saved_reviews : {type:Array,
+    default:[]},
+
+    saved_teachers : {type:Array,
+    default:[]},
+
+    saved_courses : {type:Array,
+    default:[]},
+    isdeleted : {type:Boolean,default:false}, 
+    createdby :String,
+    modifiedby:String,
+    createdat:Date,
+    modifiedat:Date
+   });
 
 
-  });
+
+
   const User = mongoose.model('User', userSchema);
 
 
@@ -120,6 +180,11 @@ const Teachers = mongoose.model('Teachers', teacherSchema);
           required: true,
           erpValidator
       },
+      name: {
+          type: String,
+          required: true,
+          validate: emptyValidator
+      },
 
       parent_id:{
         type:Number,
@@ -131,174 +196,258 @@ const Teachers = mongoose.model('Teachers', teacherSchema);
           required: true,
           validate: emptyValidator
       },
+
       rating: {
           type: Array,
-          required: true,
-          validate: emptyValidator
       },
+
       anonymous: {
           type: Boolean,
           required: true
       },
+
       teacher_id: {
           type: Number,
           required: true
       },
+
       course_id: {
           type: Number,
           required: true
-      }
+      },
+
+      upvotes: {
+          type: Number,
+          default: 0
+      },
+
+      downvotes: {
+          type: Number,
+          default: 0
+      },
+
+      createdby :String,
+      modifiedby:String,
+      createdat:Date,
+      modifiedat:Date,
+      isDeleted:{type:Boolean,default:false}
   });
   
   const Comments = mongoose.model('Comments', commentSchema);
 
 
-  const teachers = mongoose.model('Teachers');
-
   router.post("/register", async (req, res) => {
-    try {
-      const { email, password,firstname,lastname, erp } = req.body;
-      
-      // Check if the email is already in use
-      let user = await User.findOne({ email });
-      if (user) return res.status(409).json({ msg: "This email is already in use" });
-      
-      let user1 = await User.findOne({ erp });
-      if (user1) return res.status(409).json({ msg: "This ERP is already in use" });
-      
-      // Check if the password is valid
-      if (passwordValidator.validator(password)) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      
-      // Create a new user in database and respond
-      await User.create({ email, password: hashedPassword,firstname,lastname,erp});
-      return res.status(200).json({ msg: "Successful" });
+  try {
+    const { email, password, firstname, lastname, erp } = req.body;
+    
+    // Check if the email or ERP is already in use
+    const existingUserByEmail = await User.findOne({ email });
+    if (existingUserByEmail) {
+      return res.status(409).json({ msg: "This email is already in use" });
     }
-      else{
-        res.status(400).json({ 
-          msg: "Password must contain at least 1 uppercase letter, 1 number, and have a length greater than 8."
+    
+    const existingUserByErp = await User.findOne({ erp });
+    if (existingUserByErp) {
+      return res.status(409).json({ msg: "This ERP is already in use" });
+    }
+    
+    // Check if the password is valid
+    if (!passwordValidator.validator(password)) {
+      return res.status(400).json({ 
+        msg: "Password must contain at least 1 uppercase letter, 1 number, and have a length greater than 8."
       });
-              }}
-      
-      catch (error) {
-      // If the error is a validation error(MongoDB), respond with a 400 status code
-      
-        console.error(error);
-        return res.status(500).json({ msg: error.message });
-      }
+    }
+    
+    // Hash password and create user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({ 
+      email, 
+      password: hashedPassword,
+      firstname,
+      lastname,
+      erp,
+      createdby: email,
+      createdat: new Date(),
+      modifiedat: new Date()
     });
+    
+    return res.status(200).json({ msg: "Registration successful" });
+  } catch (error) {
+    console.error(error);
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ msg: "There was a validation error.", details: error.message });
+    } else if (error.code === 11000) {
+      return res.status(409).json({ msg: "There was a duplicate key error.", details: error.message });
+    } else {
+      return res.status(500).json({ msg: error.message });
+    }
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { loginUsername, password } = req.body;
+
+    // Determine if the loginUsername is an ERP or an email
+    let user;
+    if (loginUsername.includes('@')) {
+      // If loginUsername contains '@', treat it as an email
+      user = await User.findOne({ email: loginUsername });
+    } else {
+      // Otherwise, treat it as an ERP
+      user = await User.findOne({ erp: loginUsername });
+    }
+
+    if (!user) {
+      return res.status(404).json({ msg: "This ERP or Email is not linked to any account" });
+    }
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+    if (!passwordCheck) {
+      return res.status(401).json({ msg: "Incorrect Password" }); // 401 Unauthorized for wrong credentials
+    }
+
+    // Use a unique identifier for JWT; could be user's id, email, or a combination
+    const token = jwt.sign({
+      erp: user.erp, 
+      email: user.email,
+      roles: user.roles,
+      // firstname: user.firstname,
+      // lastname: user.lastname, 
+      createdAt: new Date(),
+    }, securitykey, { expiresIn: "1d" });
+
+    res.json({
+      msg: "LOGGED IN",
+      token
+    });
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ msg: "INTERNAL SERVER ERROR" }); // Ensure consistent error format
+  }
+});
 
 
+router.get('/logout', async (req, res) => {
+  try {
+    // Invalidate token by removing it from the client-side storage
+    res.clearCookie('token');
+    // Optionally, set a very short expiry for the cookie to ensure it gets deleted
+    res.status(200).json({ msg: 'Logged out successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
+});
+
+router.patch('/changepassword',authenticateToken, requireRole("User"), async (req, res) => {
+  try {
+    const {erp, oldpassword, newpassword } = req.body;
+   
+    if (oldpassword == newpassword) {
+      return res.status(400).json({ msg: "New Password cannot be same as old password" });
+    }
 
 
-    
-    router.post("/login", async (req, res) => {
-      try {
-        const { loginUsername, password } = req.body;
-    
-        // Determine if the loginUsername is an ERP or an email
-        let user;
-        if (loginUsername.includes('@')) {
-          // If loginUsername contains '@', treat it as an email
-          user = await User.findOne({ email: loginUsername });
-        } else {
-          // Otherwise, treat it as an ERP
-          user = await User.findOne({ erp: loginUsername });
-        }
-    
-        if (!user) {
-          return res.status(404).json({ msg: "This ERP or Email is not linked to any account" });
-        }
-    
-        const passwordCheck = await bcrypt.compare(password, user.password);
-        if (!passwordCheck) {
-          return res.status(401).json({ msg: "Incorrect Password" }); // 401 Unauthorized for wrong credentials
-        }
-    
-        // Use a unique identifier for JWT; could be user's id, email, or a combination
-        const token = jwt.sign({
-          id: user.erp, 
-          email: user.email, 
-          createdAt: new Date(),
-        }, securitykey, { expiresIn: "1d" });
-    
-        res.json({
-          msg: "LOGGED IN",
-          token
+       // Check if the password is valid
+       if (!passwordValidator.validator(oldpassword)) {
+        return res.status(400).json({ 
+          msg: "Old Password must contain at least 1 uppercase letter, 1 number, and have a length greater than 8."
         });
-      } catch (error) {
-        console.log(error)
-        return res.status(500).json({ msg: "INTERNAL SERVER ERROR" }); // Ensure consistent error format
-      }
-    });
-  
+  }
+ 
+ 
+    // Check if the password is valid
+       if (!passwordValidator.validator(newpassword)) {
+        return res.status(400).json({ 
+          msg: "New Password must contain at least 1 uppercase letter, 1 number, and have a length greater than 8."
+        });
+  }
+    // Attempt to find the user by ERP
+    const user = await User.findOne({ erp });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
 
-    router.post('/logout', async (req, res) => {
-      try {
-        // Invalidate token by removing it from the client-side storage
-        res.clearCookie('token');
-        // Optionally, set a very short expiry for the cookie to ensure it gets deleted
-        res.status(200).json({ msg: 'Logged out successfully' });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ msg: 'Internal server error' });
-      }
-    });
-    
-    router.patch('/changepassword', async (req, res) => {
-      try {
-        const { erp, oldpassword, newpassword } = req.body;
-        if(oldpassword==newpassword){
-          return res.status(400).json({ msg: "New Password cannot be same as old password" });
-        }
-        // Attempt to find the user by ERP
-        const user = await User.findOne({ erp });
-        if (!user) {
+    // Check that the old password matches
+    const passwordCheck = await bcrypt.compare(oldpassword, user.password);
+    if (!passwordCheck) {
+      return res.status(401).json({ msg: "Incorrect Password" }); // Unauthorized for wrong credentials
+    }
+
+    // Hash the new password before saving
+    const hashedNewPassword = await bcrypt.hash(newpassword, 10); // Assuming a salt round of 10, adjust as needed
+    user.password = hashedNewPassword;
+    await user.save();
+
+    res.status(200).json({ msg: "Password Changed Successfully" });
+  } catch (error) {
+    if (error.name === 'JsonWebTokenError') {
+      // Handle JWT-specific errors
+      console.log(error);
+      return res.status(401).json({ msg: "Invalid Token" });
+    }
+    console.error(error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+});
+
+router.get('/getuserprofile',authenticateToken, requireRole("User"),async(req,res)=>{
+  
+  const { erp } = req.user;
+  try {
+      const user = await User.findOne({ erp }, { password: 0 }); // Excluding password from the response
+      if (!user) {
           return res.status(404).json({ msg: "User not found" });
-        }
-    
-        // Check that the old password matches
-        const passwordCheck = await bcrypt.compare(oldpassword, user.password);
-        if (!passwordCheck) {
-          return res.status(401).json({ msg: "Incorrect Password" }); // Unauthorized for wrong credentials
-        }
-    
-        // Hash the new password before saving
-        const hashedNewPassword = await bcrypt.hash(newpassword, 10); // Assuming a salt round of 10, adjust as needed
-        user.password = hashedNewPassword;
-        await user.save();
-    
-        res.status(200).json({ msg: "Password Changed Successfully" });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ msg: "Internal server error" });
       }
+      res.json(user); // Respond with the user profile information
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ msg: "Internal server error" });
+  }
+
+});
+
+
+router.patch('/updateProfile', authenticateToken, async (req, res) => {
+  // Extract the desired updates from the request body
+  const { updated_firstname, updated_lastname, updated_profile_picture } = req.body;
+
+  try {
+    // Assuming authenticateToken middleware adds the decoded token to req.user
+    const erp = req.user.erp;
+
+    // Find the user by ERP
+    const user = await User.findOne({ erp });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    // Apply the updates if provided
+    if (updated_firstname) user.firstname = updated_firstname;
+    if (updated_lastname) user.lastname = updated_lastname;
+    if (updated_profile_picture) user.profile_picture = updated_profile_picture;
+
+    // Save the updated user
+    await user.save();
+
+    // Respond with a success message
+    res.status(200).json({
+      msg: "Profile updated successfully",
+      user: { erp, email: user.email, firstname: user.firstname, lastname: user.lastname, profile_picture: user.profile_picture }
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+});
 
 
 
-    router.post('/postcomment', async (req, res) => {
-      try {
-          const { teacher_id, comment, rating, course_id, anonymous, erp } = req.body;
-          const newComment = await Comments.create({ teacher_id, comment, rating, course_id, anonymous, erp });
-          
-          // Access the _id from the newly created comment
-          const objectId = newComment._id;
-  
-          res.json({ msg: "Comment Posted Successfully", objectId });
-  
-          // Print the ObjectId
-          console.log("New comment created with ObjectId:", objectId);
-      } catch(error) {
-          console.error(error);
-          res.status(500).json({ msg: "Internal server error" });
-      }
-  });
-  
 
 
 
-    router.post('/postreply', async (req, res) => {
+    router.post('/postcomment',authenticateToken,requireRole("User"), async (req, res) => {
       try {
           const { teacher_id, comment, rating, course_id, anonymous, erp, parent_id } = req.body;
           const newComment = await Comments.create({ teacher_id, comment, rating, course_id, anonymous, erp, parent_id });
@@ -316,7 +465,7 @@ const Teachers = mongoose.model('Teachers', teacherSchema);
       }
   });
   
-  router.post('/getcommentteacherandcourse', async (req, res) => {
+  router.post('/getcommentteacherandcourse',authenticateToken,requireRole("User"), async (req, res) => {
     try {
         const { teacher_id, course_id } = req.body;
         const comments = await Comments.find({ teacher_id, course_id });
@@ -328,7 +477,7 @@ const Teachers = mongoose.model('Teachers', teacherSchema);
 } );
 
 
-router.post('/getcommentteacher', async (req, res) => {
+router.post('/getcommentteacher',authenticateToken,requireRole("User"), async (req, res) => {
   try {
       const { teacher_id } = req.body;
       const comments = await Comments.find({ teacher_id});
@@ -340,7 +489,7 @@ router.post('/getcommentteacher', async (req, res) => {
 } );
 
 
-router.post('/getcommentcourse', async (req, res) => {
+router.post('/getcommentcourse',authenticateToken,requireRole("User"), async (req, res) => {
   try {
       const { course_id } = req.body;
       const comments = await Comments.find({  course_id });
@@ -352,7 +501,7 @@ router.post('/getcommentcourse', async (req, res) => {
 } );
 
 
-router.delete('/deletecomment', async (req, res) => {
+router.delete('/deletecomment',authenticateToken,requireRole("User"), async (req, res) => {
   try {
       const { objectId } = req.body;
       const deletedComment = await Comments.findByIdAndDelete(objectId);
@@ -368,21 +517,8 @@ router.delete('/deletecomment', async (req, res) => {
 );
 
 
-router.post('/getuserprofile',async(req,res)=>{
-  try{
-    const { erp } = req.body;
-    const user = await User.findOne({ erp },{ password: 0 });
-    if(!user){
-      return res.status(404).json({msg:"User not found"});
-    }
-    res.json(user);
-  }catch(err){
-    console.error(err);
-    res.status(500).json({ msg: "Internal server error" });
-  }
-});
 
-router.get('/getteachers', async (req, res) => {
+router.get('/getteachers',authenticateToken,requireRole("User"), async (req, res) => {
   try {
     const teachers = await Teachers.find({});
     res.status(200).json(teachers);
@@ -392,20 +528,20 @@ router.get('/getteachers', async (req, res) => {
   }
 });
 
-// For courses Define a schema in the databse 
-router.get('/getteachers', async (req, res) => {
-  try {
-    const teachers = await Teachers.find({});
-    res.status(200).json(teachers);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ msg: "Internal server error", error: error.message });
-  }
-});
+// // For courses Define a schema in the databse 
+// router.get('/getteachers',authenticateToken,requireRole("User"), async (req, res) => {
+//   try {
+//     const teachers = await Teachers.find({});
+//     res.status(200).json(teachers);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ msg: "Internal server error", error: error.message });
+//   }
+// });
 
 
 
-router.get('/getteachercourse', async (req, res) => {
+router.get('/getteachercourse',authenticateToken,requireRole("User"), async (req, res) => {
   try {
     const {course_id}= req.body;
     const teachers = await Teachers.find({course_id});
@@ -418,7 +554,7 @@ router.get('/getteachercourse', async (req, res) => {
 
 
 
-router.post('/createTeacher', async (req, res) => {
+router.post('/createTeacher',authenticateToken,requireRole("Moderator"), async (req, res) => {
   try {
     const { Name, Title, Email, Overview, CoursesTaught, Department, Specialization, OnboardStatus, ImageFile } = req.body;
     const newTeacher = await Teachers.create({ Name, Title, Email, Overview, CoursesTaught, Department, Specialization, OnboardStatus, ImageFile });
